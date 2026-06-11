697d44141f1afae96cbf01bc12ad2f869f28becb
Ashad <your.ashadschooladress@gmail.com>
2026-06-11 21:37:23 +0530

    Added my custom ashad.java file

---
+++ ashad.java
@@ -0,0 +1,9 @@
+public class ashad{
+    public static void main(String args [])
+    {
+        System.out.println("Hello world ");
+        int a = 5;
+        int b = 10;
+        System.out.println("the sum od a and b is " + (a+b));
+    }
+}
